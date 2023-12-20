bc6f4d61a26252ed7f9315f7a257d105d8cce74e
Palatip Jantawong <palatipjant@outlook.com>
2023-12-21 00:44:05 +0700

    Initial Commit

---
+++ Willy Nilly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Willy Nilly
+//
+//  Created by Palatip Jantawong on 21/12/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Willy Nilly/Willy_NillyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Willy_NillyApp.swift
+//  Willy Nilly
+//
+//  Created by Palatip Jantawong on 21/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Willy_NillyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
